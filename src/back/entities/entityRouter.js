fa587e8dca61e4a6765382d2e8f60c2f48bd4a11
walhs <waalky.hs@gmail.com>
2015-11-04 18:51:50 -0200

    Add delete method on router api

--- src/back/entities/entityRouter.js
+++ src/back/entities/entityRouter.js
@@ -22,5 +22,30 @@ function entityRouter(entities, accessToken) {
     response.send({name: entity.Entity.name || ''});
   });
 
+  router.delete('/:entity/:id/', function (request, response) { //localhost:3000/entities/:entity
+    try {
+      var Entity = entities[request.params.entity];
+    }catch(err){
+      response.status(400).json({
+        message: 'Entity is not defined',
+        code: 0
+      });
+    }
+    var id = request.params.id;
+    var entity = new Entity({id: id});
+
+    entity.delete()
+      .then(function () {
+        response.status(204).end();
+      })
+      .catch(function () {
+        response.status(500).json({
+          message: 'Internal error',
+          code: 0
+        });
+      });
+
+  });
+
   return router;
 }
