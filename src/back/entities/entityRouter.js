27a3b5938528791abdaf931ec004180385bc3e13
Bruno Picinin Cardoso <brunopicinin@gmail.com>
2015-11-05 18:51:19 -0200

    Add `GET /:entity/:id/` method to router

--- src/back/entities/entityRouter.js
+++ src/back/entities/entityRouter.js
@@ -22,5 +22,51 @@ function entityRouter(entities, accessToken) {
     response.send({name: entity.Entity.name || ''});
   });
 
+  router.get('/:entity/:id/', function get(req, res) {
+    var entityName = req.params.entity;
+    var id = req.params.id;
+
+    // check for errors
+    if (!entities.hasOwnProperty(entityName)) {
+      res.status(404).json({
+        code: 0,
+        message: 'Entity not defined'
+      });
+      return;
+    }
+
+    var Entity = entities[entityName];
+
+    Entity.get({id: id})
+      .then(function (entity) {
+        res.json(_objectToDocument(entity));
+      })
+      .catch(function () {
+        res.status(404).json({
+          code: 0,
+          message: 'Entity not found'
+        });
+      });
+  });
+
   return router;
+}
+
+function _objectToDocument(entityObject) {
+  var document = {};
+
+  var adapterName = entityObject.adapterName;
+  var attributes = entityObject.Entity.attributes;
+
+  for (var attrName in attributes) {
+    if (attributes.hasOwnProperty(attrName)) {
+      var attr = attributes[attrName];
+      var attrDataName = attr.getDataName(adapterName);
+      document[attrDataName] = attr.getDataValue(entityObject[attrName]);
+    }
+  }
+
+  document.Entity = entityObject.Entity.specification.name;
+
+  return document;
 }
