92388d6d5660b635ab7b686c22d1c41e936d4fbd
walhs <waalky.hs@gmail.com>
2015-11-06 18:28:55 -0200

    Revert "Merge b4a-172 with master"
    This reverts commit eaf2ed5627ae94e5aa07cd36cd8a939434671b4c.

--- src/back/entities/index.js
+++ src/back/entities/index.js
@@ -1,6 +1,6 @@
 /**
  * Contains implementation of REST API for back{4}app's entities.
- * @module back4app-rest/entities
+ * @module back4app-rest
  */
 var entityRouter = require('./entityRouter');
 
--- src/back/index.js
+++ src/back/index.js
@@ -1,5 +1,5 @@
 /**
- * Contains implementation of REST API for back{4}app.
+ * Contains implementation of REST API for back{4}app's entities.
  * @module back4app-rest
  */
 var entities = require('./entities');
