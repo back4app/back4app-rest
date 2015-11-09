ae3b3e843194a0420b00208fbfb3cb49685d8e0a
Bruno Picinin Cardoso <brunopicinin@gmail.com>
2015-11-09 14:12:27 -0200

    Change initial token implementation

--- src/back/entities/entityRouter.js
+++ src/back/entities/entityRouter.js
@@ -10,23 +10,6 @@ module.exports = entityRouter;
 function entityRouter(entities, accessToken) {
   var router = express.Router();
 
-  router.use(function (request, response, next) {
-    //access token validation
-    var token = request.headers['x-access-token'] || '';
-    if(!token)
-    {
-      response.status(403).send('Token not supplied or empty');
-    }
-    else if(token == accessToken)
-    {
-      next();
-    }
-    else
-    {
-      response.status(404).send('Invalid token');
-    }
-  });''
-
   router.use(bodyParser.json());
 
   /**
