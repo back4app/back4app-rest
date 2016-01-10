d11f66f68009363d5acd818c0519feb9245555d9
Bruno Picinin Cardoso <brunopicinin@gmail.com>
2016-01-10 19:13:54 -0200

    Fix lint issue

--- src/back/entities/entityRouter.js
+++ src/back/entities/entityRouter.js
@@ -283,7 +283,7 @@ function entityRouter(entities, accessToken) {
 
   /* 404 handler is the last non-error middleware */
 
-  router.use(function(req, res, next) {
+  router.use(function (req, res) {
     res.status(404).json({
       code: 121,
       error: 'URL Not Found'
