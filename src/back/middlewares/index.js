0d0c9acdbab4314f214c613d5e7e9b7a7740f1e5
walhs <waalky.hs@gmail.com>
2016-01-15 21:22:30 -0200

    Add session

--- src/back/middlewares/index.js
+++ src/back/middlewares/index.js
@@ -5,8 +5,10 @@
 var authentication = require('./authentication');
 var error = require('./error');
 var notfound = require('./notfound');
+var session = require('./session');
 
 module.exports = {};
 module.exports.authentication = authentication;
 module.exports.error = error;
 module.exports.notfound = notfound;
+module.exports.session = session;
